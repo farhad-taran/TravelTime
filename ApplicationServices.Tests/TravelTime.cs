0a3551714e9832cec0bbe53744377202522ae282
ftaran <farhad86@gmail.com>
2015-05-09 07:46:09 +0100

    api query added

--- ApplicationServices.Tests/TravelTime.cs
+++ ApplicationServices.Tests/TravelTime.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using TestStack.BDDfy;
@@ -17,17 +18,12 @@ namespace ApplicationServices.Tests
 
         public void GivenTravelTimeQueryHasAJobLoaction()
         {
-            travelTimeQuery.JobLocation = "";
+            travelTimeQuery.JobLocation = "W3 8pe";
         }
 
-        public void AndGivenTravelTimeQueryHasCandidatePostCodes()
+        public void AndGivenTravelTimeQueryHasValidCandidatePostCodes()
         {
-            travelTimeQuery.CandidatePostCodes = new List<string>() { "", "" };
-        }
-
-        public void AndGivenTravelTimeStartTimeJourneyIsNotInThePast()
-        {
-            travelTimeQuery.JourneyStartTime = new TimeSpan();
+            travelTimeQuery.CandidatePostCodes = new List<string>() { "w3 8pe", "w3 8pe" };
         }
 
         public void WhenTravelTimeQueryIsProcessedByTravelTimeService()
@@ -49,9 +45,37 @@ namespace ApplicationServices.Tests
 
     internal class TravelTimeService
     {
+        Regex postCodeRegex =
+                new Regex(
+                    @"^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$");
+
+        public class TravelTimeApiQuery
+        {
+            public string app_id { get; set; }
+            public string app_key { get; set; }
+            public int travel_time { get; set; }
+            public double[] origin { get; set; }
+            public double points { get; set; }
+            public string[] modes { get; set; }
+            public DateTime start_time { get; set; }
+            public string[] properties { get; set; }
+        }
+
         public TravelTimeDto GetTravelTime(TravelTimeQuery travelTimeQuery)
         {
-            return new TravelTimeDto();
+            TravelTimeApiQuery apiQuery = new TravelTimeApiQuery();
+            apiQuery.origin = GetJobLocationPoint(travelTimeQuery.JobLocation);
+            apiQuery.points = GetCandidatePoints(travelTimeQuery.CandidatePostCodes);
+        }
+
+        private double GetCandidatePoints(List<string> candidatePostCodes)
+        {
+            var validPostcodes = candidatePostCodes.Where(x => postCodeRegex.IsMatch(x));
+        }
+
+        private double[] GetJobLocationPoint(string jobLocation)
+        {
+            return new double[]{51.50511021989938,-0.2666956743415083};
         }
     }
 
---
+++ IGeoliseWebApplication/Installers/HttpClientInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace IGeoliseWebApplication.Installers
+{
+    public class HttpClientInstaller:IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For(typeof (HttpClient)).UsingFactoryMethod(() => new HttpClient()));
+        }
+    }
+}
